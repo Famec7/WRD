8bbefabe8d89cd71cc8804b2d7ef0a31e5acb700
Famec <power4417@gmail.com>
2024-08-15 10:35:45 +0900

    feat: 오브젝트 풀 기능 추가 및 Projectile 속도 변경
    - 풀 미리 생성할지 여부 체크 가능
    - Projectile 속도 변경 완료 추후 변경 가능

--- Assets/Script/Projectile/Projectile.cs
+++ Assets/Script/Projectile/Projectile.cs
@@ -1,12 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Projectile : ProjectileBase
 {
     public virtual Vector3 Target { get; set; }
-    
+
+    private float _startTime;
+    private Vector3 _startPosition;
+    private float _journeyLength;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        _journeyLength = Vector3.Distance(_startPosition, Target);
+        _startTime = Time.time;
+    }
+
     protected override void MoveToTarget()
     {
-        transform.position = Vector3.Lerp(this.transform.position, Target, curve.Evaluate(Time.time));
+        float timeSinceStart = Time.time - _startTime;
+        float fractionOfJourney = timeSinceStart / _journeyLength;
+        float easedFraction = curve.Evaluate(fractionOfJourney);
+        
+        transform.position = Vector3.Lerp(_startPosition, Target, easedFraction);
     }
 
     public override void GetFromPool()
