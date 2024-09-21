da8111377861ff371cc51c53f6e6bd0b93f1f66d
Famec <power4417@gmail.com>
2024-09-21 22:47:14 +0900

    fix: 메이스 내려찍기 이펙트 출력 버그 수정

--- Assets/Script/Skill/Passive/HeavyBlow.cs
+++ Assets/Script/Skill/Passive/HeavyBlow.cs
@@ -6,36 +6,42 @@ public class HeavyBlow : PassiveSkillBase
     {
         if (!CheckTrigger() || target == null)
             return false;
-        
+
         Vector3 targetPosition = target.transform.position;
         var targets = RangeDetectionUtility.GetAttackTargets(targetPosition, Data.Range, default, targetLayer);
-        
+
         if (targets.Count == 0)
             return false;
 
+        float _range = Data.Range / 2;
+        
+        ParticleEffect effect = EffectManager.Instance.CreateEffect<ParticleEffect>("HeavyBlowEffect");
+        effect.SetPosition(targetPosition);
+        effect.SetScale(new Vector3(_range, _range, _range));
+        effect.PlayEffect();
+
         foreach (var tar in targets)
         {
-            if(tar.TryGetComponent(out Monster monster))
+            if (tar.TryGetComponent(out Monster monster))
             {
                 monster.HasAttacked(Data.GetValue(0));
-                
+
                 float slowDuration = Data.GetValue(1);
-                StatusEffectManager.Instance.AddStatusEffect(monster.status, new SlowDown(monster.gameObject, 100f, slowDuration));
-                
-                ParticleEffect effect = EffectManager.Instance.CreateEffect<ParticleEffect>("HeavyBlowEffect");
-                effect.SetPosition(targetPosition);
-                effect.PlayEffect();
+                StatusEffectManager.Instance.AddStatusEffect(monster.status,
+                    new SlowDown(monster.gameObject, 100f, slowDuration));
+
 
                 StatusEffect woundEffect = StatusEffectManager.Instance.GetStatusEffect(monster.status, typeof(Wound));
-                
-                if(woundEffect != null)
+
+                if (woundEffect != null)
                 {
                     monster.HasAttacked(Data.GetValue(2));
-                    
+
                     slowDuration = Data.GetValue(3);
                     float slowRate = Data.GetValue(4);
-                    
-                    StatusEffectManager.Instance.AddStatusEffect(monster.status, new SlowDown(monster.gameObject, slowRate, slowDuration));
+
+                    StatusEffectManager.Instance.AddStatusEffect(monster.status,
+                        new SlowDown(monster.gameObject, slowRate, slowDuration));
                 }
             }
         }
