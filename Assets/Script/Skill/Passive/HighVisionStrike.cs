9f99e221faec03ed44a0d3be5e945964f36651af
Famec <power4417@gmail.com>
2024-09-21 21:57:33 +0900

    feat: 상급 보주 무기 추가
    - 스킬 이펙트까지 구현

---
+++ Assets/Script/Skill/Passive/HighVisionStrike.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class HighVisionStrike : PassiveSkillBase
+{
+    public override bool Activate(GameObject target = null)
+    {
+        if (!CheckTrigger() || target == null)
+            return false;
+        
+        Vector3 targetPosition = target.transform.position;
+        var targets = RangeDetectionUtility.GetAttackTargets(targetPosition, Data.Range, default, LayerMaskManager.Instance.MonsterLayerMask);
+        
+        foreach (var tar in targets)
+        {
+            if (tar.TryGetComponent(out Monster monster))
+            {
+                monster.HasAttacked(Data.GetValue(0));
+                StatusEffectManager.Instance.AddStatusEffect(monster.status, new SlowDown(monster.gameObject, 100f, Data.GetValue(1)));
+                
+                ParticleEffect effect = EffectManager.Instance.CreateEffect<ParticleEffect>("HighOrbHit");
+                effect.SetPosition(target.transform.position);
+                
+                Vector3 range = new Vector3(Data.Range, Data.Range, Data.Range);
+                effect.SetScale(range);
+                effect.PlayEffect();
+            }
+        }
+        
+        return true;
+    }
+}
--- Assets/Script/Skill/PassiveAura/HighDomainExpansion.cs
+++ Assets/Script/Skill/PassiveAura/HighDomainExpansion.cs
@@ -2,46 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LowDomainExpansion : PassiveAuraSkillBase
+public class HighDomainExpansion : PassiveAuraSkillBase
 {
-    private CircleCollider2D _collider;
-    private float _slowDownValue;
-
     private List<Status> _statusList = new List<Status>();
-
+    
     protected override void Init()
     {
         base.Init();
-
-        // 범위 조정
-        _collider = GetComponent<CircleCollider2D>();
-        _collider.isTrigger = true;
-        _collider.radius = Data.Range * 16;
-
-        // 슬로우 비율 값
-        _slowDownValue = Data.GetValue(0);
+        
+        CircleCollider2D collider = GetComponent<CircleCollider2D>();
+        collider.isTrigger = true;
+        collider.radius = Data.Range * 16;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Status status))
         {
-            StatusEffectManager.Instance.AddStatusEffect(status, new SlowDown(status.gameObject, _slowDownValue));
-
+            StatusEffectManager.Instance.AddStatusEffect(status, new SlowDown(status.gameObject, Data.GetValue(0)));
+            
             _statusList.Add(status);
         }
     }
-
+    
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.TryGetComponent(out Status status))
         {
             StatusEffectManager.Instance.RemoveStatusEffect(status, typeof(SlowDown));
-
+            
             _statusList.Remove(status);
         }
     }
-
+    
     private void OnDisable()
     {
         foreach (var status in _statusList)
