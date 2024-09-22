37721c1207824e33905a5af65b7c4c90d34c13d6
Famec <power4417@gmail.com>
2024-09-22 19:17:19 +0900

    feat: 라이플 무기 추가
    - 패시브 오라 스킬까지 구현

---
+++ Assets/Script/Skill/PassiveAura/Aiming.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Aiming : PassiveAuraSkillBase
+{
+    private WaitForSeconds _waitTime;
+    private float _markDuration;
+    private float _range;
+    
+    protected override void Init()
+    {
+        base.Init();
+        
+        CircleCollider2D collider = gameObject.AddComponent<CircleCollider2D>();
+        collider.isTrigger = true;
+        collider.radius = Data.Range;
+        
+        _waitTime = new WaitForSeconds(Data.GetValue(0));
+        _markDuration = Data.GetValue(2);
+        _range = Data.Range;
+    }
+
+    private IEnumerator IE_Aim()
+    {
+        while (true)
+        {
+            yield return _waitTime;
+            
+            ApplyMarkBuff();
+        }
+    }
+
+    private void ApplyMarkBuff()
+    {
+        var targets = RangeDetectionUtility.GetAttackTargets(transform.position, _range, 360f, targetLayer);
+        
+        foreach (var target in targets)
+        {
+            if (target.TryGetComponent(out Status status))
+            {
+                StatusEffectManager.Instance.AddStatusEffect(status, new Mark(status.gameObject, _markDuration));
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(IE_Aim());
+    }
+    
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+}
