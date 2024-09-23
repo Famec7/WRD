302177a3dc6a6b9b32f732385996502dd541c6e3
Famec <power4417@gmail.com>
2024-09-23 09:27:07 +0900

    feat: 라이플 액티브 스킬 스크립트 생성
    - 혹시 모를 노트북 작업을 위해 커밋

---
+++ Assets/Script/Skill/Active/02ClickType/Fire.cs
@@ -0,0 +1,24 @@
+﻿public class Fire : ClickTypeSkill
+{
+    protected override void OnActiveEnter()
+    {
+        if (weapon.owner.Target is null)
+            return;
+        
+        if(weapon.owner.Target.TryGetComponent(out Monster monster))
+        {
+            // Todo: 단일 공격
+        }
+    }
+
+    protected override INode.ENodeState OnActiveExecute()
+    {
+        IsActive = false;
+        return INode.ENodeState.Success;
+    }
+
+    protected override void OnActiveExit()
+    {
+        ;
+    }
+}
