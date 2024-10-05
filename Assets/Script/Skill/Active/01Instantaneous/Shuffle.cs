86bc8fa70351c5351a478a99cbd32c9db14acb97
Famec <power4417@gmail.com>
2024-10-05 21:27:43 +0900

    feat: 클로버잭 스킬 추가 (이펙트 미구현)

---
+++ Assets/Script/Skill/Active/01Instantaneous/Shuffle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Shuffle : InstantaneousSkill
+{
+    private enum CardType
+    {
+        CloverJack,
+        HeartQueen,
+        DiamondKing,
+        Joker
+    }
+    
+    private CardEffectBase _cardEffect;
+    
+    protected override void OnActiveEnter()
+    {
+        // (클로버 잭(Data{0}), 하트 퀸(Data{1}), 다이아몬드 킹(Data{2}), 조커: Data{3}) 중 하나를 랜덤하게 선택
+        int random = Random.Range(0, 100);
+
+        _cardEffect.Weapon = weapon;
+        Data.CoolTime = _cardEffect.Data.CoolTime;
+        
+        _cardEffect.OnEnter();
+    }
+
+    protected override INode.ENodeState OnActiveExecute()
+    {
+        _cardEffect.OnUpdate();
+        return INode.ENodeState.Running;
+    }
+
+    protected override void OnActiveExit()
+    {
+        _cardEffect.OnExit();
+        _cardEffect = null;
+    }
+}
---
+++ Assets/Script/Skill/Active/01Instantaneous/ShuffleCard/CloverJackEffect.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class CloverJackEffect : CardEffectBase
+{
+    protected CloverJackEffect(WeaponBase weapon) : base(weapon)
+    {
+        Data = SkillManager.Instance.GetActiveSkillData("shuffle - clover jack");
+    }
+
+    public override void OnEnter()
+    {
+        Weapon.enabled = false;
+    }
+
+    public override void OnUpdate()
+    {
+        if (Weapon.owner.Target is null)
+        {
+            return;
+        }
+        
+        Vector3 targetPosition = Weapon.owner.Target.transform.position;
+        Vector3 range = new Vector3(3, 5, 0);
+        var layer = LayerMaskManager.Instance.MonsterLayerMask;
+
+        var targets = RangeDetectionUtility.GetAttackTargets(targetPosition, range, default, layer);
+        foreach (var target in targets)
+        {
+            if (target.TryGetComponent(out Monster monster))
+            {
+                monster.HasAttacked(Data.GetValue(0));
+
+                float damageAmplification = Data.GetValue(2);
+                float duration = Data.GetValue(1);
+                StatusEffect amplification = new DamageAmplification(monster.gameObject, damageAmplification, duration);
+                
+                StatusEffectManager.Instance.AddStatusEffect(monster.status, amplification);
+            }
+        }
+    }
+
+    public override void OnExit()
+    {
+        Weapon.enabled = true;
+    }
+}
