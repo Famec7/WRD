0239949da5021e7bc41007406ee14470f3bcad70
Famec <power4417@gmail.com>
2024-10-31 21:42:54 +0900

    refacot: CoroutineHandler에서 필요없는 코드 제거

--- Assets/Script/Utility/CoroutineHandler.cs
+++ Assets/Script/Utility/CoroutineHandler.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class CoroutineHandler : Singleton<CoroutineHandler>
 {
-    private readonly HashSet<Coroutine> _activeCoroutines = new HashSet<Coroutine>();
-    
     protected override void Init()
     {
         ;
@@ -14,16 +12,13 @@ public class CoroutineHandler : Singleton<CoroutineHandler>
 
     public new Coroutine StartCoroutine(IEnumerator routine)
     {
-        if(routine == null)
+        if (routine == null)
         {
             return null;
         }
-        
+
         Coroutine coroutine = base.StartCoroutine(routine);
-        
-        if(coroutine != null)
-            _activeCoroutines.Add(coroutine);
-        
+
         return coroutine;
     }
 
@@ -33,29 +28,20 @@ public class CoroutineHandler : Singleton<CoroutineHandler>
         {
             return;
         }
-        
-        if(_activeCoroutines.Contains(routine))
-        {
-            base.StopCoroutine(routine);
-            _activeCoroutines.Remove(routine);
-        }
+
+        base.StopCoroutine(routine);
     }
-    
+
     public new void StopAllCoroutines()
     {
-        foreach (var coroutine in _activeCoroutines)
-        {
-            base.StopCoroutine(coroutine);
-        }
-        
-        _activeCoroutines.Clear();
+        base.StopAllCoroutines();
     }
 
     private void OnDestroy()
     {
         StopAllCoroutines();
     }
-    
+
     private void OnApplicationQuit()
     {
         StopAllCoroutines();
