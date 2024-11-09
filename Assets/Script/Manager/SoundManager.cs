0a98f59c545628aabf0c6f25075b44ab2d83ffca
Famec <power4417@gmail.com>
2024-11-09 22:55:05 +0900

    feat: SoundManager 클래스 추가 및 기본적인 함수 구현

--- Assets/Script/Manager/SoundManager.cs
+++ Assets/Script/Manager/SoundManager.cs
@@ -1,7 +1,101 @@
-﻿namespace Script.Manager
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
 {
-    public class SoundManager
+    protected override void Init()
     {
+        _bgmSource = CreateAudioSource("BGM");
         
+        for (int i = 0; i < _sfxSourceCount; i++)
+        {
+            _sfxSources.Enqueue(CreateAudioSource($"SFX_{i}"));
+        }
     }
+    
+    private AudioSource CreateAudioSource(string sourceName)
+    {
+        GameObject audioObject = new GameObject(sourceName);
+        audioObject.transform.SetParent(transform);
+        return audioObject.AddComponent<AudioSource>();
+    }
+
+    #region BGM
+    
+    private AudioSource _bgmSource;
+
+    /// <summary>
+    /// BGM을 재생하는 함수
+    /// </summary>
+    /// <param name="clip"> 재생할 AudioClip </param>
+    public void PlayBGM(AudioClip clip)
+    {
+        if (clip == null)
+        {
+            return;
+        }
+        
+        _bgmSource.clip = clip;
+        _bgmSource.loop = true;
+        _bgmSource.Play();
+    }
+    
+    /// <summary>
+    /// BGM을 일시정지하는 함수
+    /// </summary>
+    public void PauseBGM()
+    {
+        _bgmSource.Pause();
+    }
+
+    /// <summary>
+    /// BGM을 정지하는 함수
+    /// </summary>
+    public void StopBGM()
+    {
+        _bgmSource.Stop();
+    }
+
+    #endregion
+
+    #region SFX
+
+    private const int _sfxSourceCount = 10;
+    private readonly Queue<AudioSource> _sfxSources = new Queue<AudioSource>();
+    
+    public void PlaySFX(AudioClip clip, float volume = 1.0f)
+    {
+        AudioSource sfxSource = _sfxSources.Dequeue();
+        
+        if (clip == null)
+        {
+            return;
+        }
+        
+        sfxSource.volume = volume;
+        sfxSource.PlayOneShot(clip);
+        
+        _sfxSources.Enqueue(sfxSource);
+    }
+    
+    public void StopSFX(AudioClip clip)
+    {
+        foreach (AudioSource sfxSource in _sfxSources)
+        {
+            if (sfxSource.clip == clip)
+            {
+                sfxSource.Stop();
+            }
+        }
+    }
+    
+    public void StopAllSFX()
+    {
+        foreach (AudioSource sfxSource in _sfxSources)
+        {
+            sfxSource.Stop();
+        }
+    }
+
+    #endregion
 }
