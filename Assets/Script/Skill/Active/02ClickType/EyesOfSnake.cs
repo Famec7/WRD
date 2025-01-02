c09b9efb809f8216b2baf0a59891985f89fb2e66
Famec <power4417@gmail.com>
2025-01-02 21:24:52 +0900

    feat: 뱀의 주시 스킬 구현

--- Assets/Script/Skill/Active/02ClickType/EyesOfSnake.cs
+++ Assets/Script/Skill/Active/02ClickType/EyesOfSnake.cs
@@ -7,6 +7,14 @@
         if (target.TryGetComponent(out Monster monster))
         {
             float duration = Data.GetValue(0);
+            float damageAmplification = Data.GetValue(1) / 100.0f;
+            float slowRate = Data.GetValue(2);
+            
+            StatusEffect damageAmplificationEffect = new DamageAmplification(monster.gameObject, damageAmplification, duration);
+            StatusEffectManager.Instance.AddStatusEffect(monster.status, damageAmplificationEffect);
+            
+            StatusEffect slowEffect = new SlowDown(monster.gameObject, slowRate, duration);
+            StatusEffectManager.Instance.AddStatusEffect(monster.status, slowEffect);
         }
     }
 
