2bfde97205309d5ac8bc0a0891ff0888007230d8
Famec <power4417@gmail.com>
2025-01-02 19:48:14 +0900

    feat: 케리케이온 무기 추가

---
+++ Assets/Script/Skill/Active/02ClickType/EyesOfSnake.cs
@@ -0,0 +1,23 @@
+﻿public class EyesOfSnake : ClickTypeSkill
+{
+    protected override void OnActiveEnter()
+    {
+        FindTarget();
+
+        if (target.TryGetComponent(out Monster monster))
+        {
+            float duration = Data.GetValue(0);
+        }
+    }
+
+    protected override INode.ENodeState OnActiveExecute()
+    {
+        IsActive = false;
+        return INode.ENodeState.Success;
+    }
+
+    protected override void OnActiveExit()
+    {
+        ;
+    }
+}
---
+++ Assets/Script/Skill/Effect/Wind.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    private Action<Monster> _onWindEnd;
+
+    private float _radius;
+    private Vector3 _targetPosition;
+    
+    public void Init(float radius, Vector3 targetPosition, Action<Monster> onWindEnd)
+    {
+        _radius = radius;
+        _targetPosition = targetPosition;
+        _onWindEnd = onWindEnd;
+    }
+    
+    public void Play()
+    {
+        this.transform.position = _targetPosition;
+        this.gameObject.SetActive(true);
+        
+        // radius 범위 안의 적들을 중앙으로 끌어당김
+        LayerMask layer = LayerMaskManager.Instance.MonsterLayerMask;
+        var targets = RangeDetectionUtility.GetAttackTargets(transform.position, _radius, default, layer);
+        foreach (var target in targets)
+        {
+            if (target.TryGetComponent(out Monster monster))
+            {
+                StartCoroutine(IE_MoveMonster(monster));
+            }
+        }
+    }
+
+    public void Stop(Monster monster)
+    {
+        this.gameObject.SetActive(false);
+        _onWindEnd?.Invoke(monster);
+    }
+    
+    // 몬스터의 위치를 특정 위치로 이동하는 코루틴
+    private IEnumerator IE_MoveMonster(Monster monster)
+    {
+        while ((monster.transform.position - _targetPosition).sqrMagnitude > Mathf.Epsilon)
+        {
+            monster.transform.position = Vector3.MoveTowards(monster.transform.position, _targetPosition, 0.1f);
+            yield return null;
+        }
+        
+        Stop(monster);
+    }
+}
