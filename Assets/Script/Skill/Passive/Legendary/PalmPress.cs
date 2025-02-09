ae397844a60e5c30dc649f7bb416dd0b4903191a
Famec <power4417@gmail.com>
2025-02-09 20:06:41 +0900

    feat: 붓다글러브 무기 추가 및 패시브 스킬(항마장) 추가

---
+++ Assets/Script/Skill/Passive/Legendary/PalmPress.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class PalmPress : PassiveSkillBase
+{
+    private float _damage;
+    private float _stunDuration;
+
+    protected override void Init()
+    {
+        base.Init();
+
+        _damage = Data.GetValue(0);
+        _stunDuration = Data.GetValue(1);
+    }
+
+    public override bool Activate(GameObject target = null)
+    {
+        if (!CheckTrigger())
+        {
+            return false;
+        }
+
+        LayerMask monsterLayer = LayerMaskProvider.MonsterLayerMask;
+        var targets = RangeDetectionUtility.GetAttackTargets(transform.position, Data.Range, 360.0f, monsterLayer);
+
+        foreach (var tar in targets)
+        {
+            if (tar.TryGetComponent(out Monster monster))
+            {
+                monster.HasAttacked(_damage);
+
+                if (HasWound(monster))
+                {
+                    ApplyStun(monster);
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // 자상 디버프가 있으면 자상을 제거하고 true 반환, 없으면 false 반환
+    private bool HasWound(Monster monster)
+    {
+        StatusEffect wound = StatusEffectManager.Instance.GetStatusEffect(monster.status, typeof(Wound));
+
+        if (wound is null)
+        {
+            return false;
+        }
+
+        StatusEffectManager.Instance.RemoveStatusEffect(monster.status, typeof(Wound));
+        return true;
+    }
+
+    private void ApplyStun(Monster monster)
+    {
+        StatusEffect stun = new Stun(monster.gameObject, _stunDuration);
+        StatusEffectManager.Instance.AddStatusEffect(monster.status, stun);
+    }
+}
