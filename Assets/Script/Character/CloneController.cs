cade6e4665be5af131aaa84334c35095b786a991
Famec <power4417@gmail.com>
2025-03-25 17:00:07 +0900

    feat: 주변에 적이 없으면 분신이 사라지도록 수정

--- Assets/Script/Character/CloneController.cs
+++ Assets/Script/Character/CloneController.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CloneController : CharacterController
 {
+    private float _spawnTime = 0.0f;
+    private const float DespawnTime = 1.0f;
+    
     public void Spawn(Vector2 position)
     {
         this.transform.position = position;
@@ -13,6 +17,7 @@ public class CloneController : CharacterController
     public void Despawn()
     {
         this.gameObject.SetActive(false);
+        _spawnTime = 0.0f;
     }
     
     public override void AttachWeapon(WeaponBase weapon)
@@ -23,5 +28,17 @@ public class CloneController : CharacterController
     public override void DetachWeapon()
     {
         ;
+    }
+
+    private void Update()
+    {
+        if (_spawnTime > DespawnTime)
+        {
+            Despawn();
+        }
+        else
+        {
+            _spawnTime += Time.deltaTime;
+        }
     }
 }
