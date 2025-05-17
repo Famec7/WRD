7539ee022fc0c0182dee2ee06482d827cfca896d
Famec <power4417@gmail.com>
2025-05-17 14:09:23 +0900

    fix: 발리스타 선고 오류 수정

--- Assets/Script/Skill/Passive/Epic/Condemn.cs
+++ Assets/Script/Skill/Passive/Epic/Condemn.cs
@@ -4,32 +4,35 @@ public class Condemn : PassiveSkillBase
 {
     public override bool Activate(GameObject target = null)
     {
-        if(!CheckTrigger() || target == null)
+        if (!CheckTrigger() || target == null)
             return false;
-        
+
         if (target.TryGetComponent(out Status status))
         {
-            float stunDuration = Data.GetValue(0);
+            float stunDuration = Data.GetValue(1);
             StatusEffectManager.Instance.AddStatusEffect(status, new Stun(status.gameObject, stunDuration));
-            
+
             var markStatus = StatusEffectManager.Instance.GetStatusEffect(status, typeof(Mark));
-            if (markStatus != null)
+
+            if (status.TryGetComponent(out Monster monster))
             {
-                if(status.TryGetComponent(out Monster monster))
+                monster.HasAttacked(Data.GetValue(0));
+                if (markStatus != null)
                 {
                     GuidedProjectile projectile = ProjectileManager.Instance.CreateProjectile<GuidedProjectile>(default, this.transform.position);
                     projectile.Target = target.gameObject;
-                    
-                    projectile.OnHit += () => OnHit(monster, Data.GetValue(2));
+
+                    projectile.OnHit += () => OnHit(monster, Data.GetValue(3));
                 }
             }
-            
-            StatusEffectManager.Instance.AddStatusEffect(status, new Mark(status.gameObject, Data.GetValue(1)));
+
+
+            StatusEffectManager.Instance.AddStatusEffect(status, new Mark(status.gameObject, Data.GetValue(2)));
         }
 
         return true;
     }
-    
+
     protected virtual void OnHit(Monster monster, float damage)
     {
         monster.HasAttacked(damage);
