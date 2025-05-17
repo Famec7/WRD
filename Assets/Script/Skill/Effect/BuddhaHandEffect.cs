85cfea4816df1b6a85e07eaeddd263bab4a7994c
Famec <power4417@gmail.com>
2025-05-17 22:13:54 +0900

    feat: 여래신장 액티브 이펙트 추가

---
+++ Assets/Script/Skill/Effect/BuddhaHandEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BuddhaHandEffect : AnimationEffect
+{
+    [SerializeField]
+    private ParticleSystem _effect;
+    
+    public void PlayGroundEffect()
+    {
+        if (_effect != null)
+        {
+            _effect.Play();
+        }
+    }
+
+    public override void StopEffect()
+    {
+        if (_effect != null)
+        {
+            _effect.Stop();
+        }
+        
+        gameObject.SetActive(false);
+    }
+}
