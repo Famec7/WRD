61117f54fe23f7c78c4cd6b08455781dd1073688
Famec <power4417@gmail.com>
2025-05-18 12:50:14 +0900

    feat: 조커존 추가 및 스킬 ID 변경

---
+++ Assets/Script/Skill/PassiveAura/Legendary/JokerArea.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class JokerArea : PassiveAuraSkillBase
+{
+    [SerializeField] private SlowZone _slowZone;
+
+    protected override void Init()
+    {
+        base.Init();
+        _slowZone.SetData(0, Data.Range, Data.GetValue(0));
+    }
+
+    private void OnEnable()
+    {
+        _slowZone.PlayEffect();
+    }
+    
+    private void OnDisable()
+    {
+        _slowZone.StopEffect();
+    }
+}
