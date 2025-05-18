abf1e800b87ec9db6b830e8e8e6ea1b5d7638fd0
Famec <power4417@gmail.com>
2025-05-18 20:49:56 +0900

    feat: 씬 로드시 초기화하는 오브젝트 추가

---
+++ Assets/Script/Scene/GameScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameScene : SceneBase
+{
+    [SerializeField]
+    private AudioClip bgm;
+    
+    protected override void Initialize()
+    {
+        if (bgm != null)
+        {
+            SoundManager.Instance.PlayBGM(bgm);
+        }
+    }
+
+    protected override void Cleanup()
+    {
+        ;
+    }
+}
---
+++ Assets/Script/Scene/SceneBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public abstract class SceneBase : MonoBehaviour
+{
+    private void Awake()
+    {
+        Initialize();
+    }
+    
+    private void OnDestroy()
+    {
+        Cleanup();
+    }
+
+    /// <summary>
+    /// 씬을 초기화합니다. (Awake에서 호출)
+    /// </summary>
+    protected abstract void Initialize();
+
+    /// <summary>
+    /// 씬을 정리합니다. (OnDestroy에서 호출)
+    /// </summary>
+    protected abstract void Cleanup();
+}
