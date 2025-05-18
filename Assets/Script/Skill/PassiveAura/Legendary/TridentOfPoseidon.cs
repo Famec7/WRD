542269f6bfa11e7ef997bfed0d441cd65c787f60
Famec <power4417@gmail.com>
2025-05-18 12:30:38 +0900

    feat: 권총 패시브 추가 및 CSV 트리아이나 패시브 삭제

--- Assets/Script/Skill/PassiveAura/Legendary/TridentOfPoseidon.cs
+++ Assets/Script/Skill/PassiveAura/Legendary/TridentOfPoseidon.cs
@@ -1,3 +1,10 @@
-﻿public class TridentOfPoseidon : PassiveAuraSkillBase
+﻿using UnityEngine;
+
+public class TridentOfPoseidon : PassiveAuraSkillBase
 {
+    [Header("이속 감소 범위")]
+    [SerializeField]
+    private float _passiveRange = 1.0f;
+
+    [SerializeField] private SlowZone _slowZone;
 }
