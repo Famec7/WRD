f5d1b4116f5eefb2cae54474c14de498afcf34f9
Famec <power4417@gmail.com>
2025-05-20 21:36:13 +0900

    feat: 테스트 버튼 추가

---
+++ Assets/Script/UI/TestPannel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TestPannel : MonoBehaviour
+{
+    [SerializeField] private GameObject testButton;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            testButton.SetActive(!testButton.activeSelf);
+        }
+    }
+}
