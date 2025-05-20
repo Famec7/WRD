c35832496f199ee351d2e3932cc1292c7174640b
Famec <power4417@gmail.com>
2025-05-20 11:15:04 +0900

    fix: Stone projectile 추가

---
+++ Assets/Script/Weapon/Stone.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Stone : RangedWeapon
+{
+    protected override void Attack()
+    {
+        if (owner.Target.TryGetComponent(out Monster monster))
+        {
+            GuidedProjectile projectile = ProjectileManager.Instance.CreateProjectile<GuidedProjectile>("Stone", this.transform.position);
+            
+            projectile.Target = monster.gameObject;
+            projectile.SetType(type);
+            projectile.OnHit += () => OnHit(monster, Data.AttackDamage);
+            
+            PlayAttackSound();
+        }
+    }
+}
